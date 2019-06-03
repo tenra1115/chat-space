18b9f447bcb9a6e9e5b482851205a4407bba4030
tenra1115 <katsuten1115@icloud.com>
2019-06-03 13:44:35 +0900

    buildup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.new
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+         has_many :groups, through: :group_users
+         has_many :group_users
+        accepts_nested_attributes_for :group_users, allow_destroy: true
+
 end
