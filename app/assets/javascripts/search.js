d729cb3e7162513c19ba63ebb1d6fb9ccdd326a4
tenra1115 <katsuten1115@icloud.com>
2019-06-10 20:32:09 +0900

    &-remake

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,7 +6,7 @@ $(function() {
     $('p').data('name', 'user_id')
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
                 </div>`
     search_list.append(html);      
   }
@@ -25,7 +25,7 @@ $(function() {
                   <div class="chat-group-user clearfix" id="chat-group-user-22">
                     <input name="group[user_ids][]" type="hidden" value=${user_id}>
                       <p class="chat-group-user__name">${name}</p>
-                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                   </div>
                 </div>
                 `
